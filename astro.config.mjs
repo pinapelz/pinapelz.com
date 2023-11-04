6e2aa7159f2dabe28ecde23409c354ca4814b6e2
Pinapelz <yukais@pinapelz.com>
2023-11-04 16:51:12 -0700

    fix: cloudflare pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: 'public',
-  publicDir: 'static',
   integrations: [tailwind()]
 });
